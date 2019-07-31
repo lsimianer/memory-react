caf5522bc7a75443bd7da4954e8b06372aa90ddf
lukas simianer <lsimianer@gmail.com>
2019-07-30 23:31:04 -0500

    Updates

---
+++ precache-manifest.5f1544fbbbeb324847c51546bc7dcd7e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d07090f37a7f0e067e8809c5a0011a2",
+    "url": "/memory-react/index.html"
+  },
+  {
+    "revision": "97204d2bff26ab1e38a7",
+    "url": "/memory-react/static/css/main.ca9eff02.chunk.css"
+  },
+  {
+    "revision": "015be0247dc3ee2955cc",
+    "url": "/memory-react/static/js/2.76d8ed81.chunk.js"
+  },
+  {
+    "revision": "97204d2bff26ab1e38a7",
+    "url": "/memory-react/static/js/main.d4521f5c.chunk.js"
+  },
+  {
+    "revision": "39a3745a50e89c0b0ea4",
+    "url": "/memory-react/static/js/runtime~main.7c494fe2.js"
+  },
+  {
+    "revision": "2043fdd72e25614ef2eff6415d0780fb",
+    "url": "/memory-react/static/media/p1.2043fdd7.png"
+  },
+  {
+    "revision": "c79173065f4c3b65bcdcb495be5af018",
+    "url": "/memory-react/static/media/p10.c7917306.png"
+  },
+  {
+    "revision": "9acd971cd256a2f6cf7b55fbbf21308e",
+    "url": "/memory-react/static/media/p11.9acd971c.png"
+  },
+  {
+    "revision": "0f85bf42ac4faaa268c662ab561aec8d",
+    "url": "/memory-react/static/media/p12.0f85bf42.png"
+  },
+  {
+    "revision": "14e0db5fa3947b9d1ea126b77c2263bd",
+    "url": "/memory-react/static/media/p2.14e0db5f.png"
+  },
+  {
+    "revision": "543afab6142f474f5627398d63fdb777",
+    "url": "/memory-react/static/media/p3.543afab6.png"
+  },
+  {
+    "revision": "4e4460cadf1db770756f889ad665f0ca",
+    "url": "/memory-react/static/media/p4.4e4460ca.png"
+  },
+  {
+    "revision": "0eddb360915cf43e96bbdd7de65e2f16",
+    "url": "/memory-react/static/media/p5.0eddb360.png"
+  },
+  {
+    "revision": "77d444442b621369e66aac6d27414708",
+    "url": "/memory-react/static/media/p6.77d44444.png"
+  },
+  {
+    "revision": "70b12c71000f84d1b7f98da8ae0ee064",
+    "url": "/memory-react/static/media/p7.70b12c71.png"
+  },
+  {
+    "revision": "8121c8fab042ab86be61ce434bc62149",
+    "url": "/memory-react/static/media/p8.8121c8fa.png"
+  },
+  {
+    "revision": "477317b04d7db015dd26e0dd5d9e228a",
+    "url": "/memory-react/static/media/p9.477317b0.png"
+  }
+]);
